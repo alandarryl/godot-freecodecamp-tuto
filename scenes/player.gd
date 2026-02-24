f0345d03265391002d24e81809eeb35aab7c22ac
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-24 20:26:47 +0100

    impliment double jump

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var initial_jump_count = 2
+var jump_count = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,10 +14,15 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		jump_count = initial_jump_count
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if  jump_count > 0 or is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jump_count -= 1
+			print(jump_count)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
